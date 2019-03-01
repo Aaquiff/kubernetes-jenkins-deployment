df6610c8d2c7032b17f1f6162e118c200f9a1361
Aaquiff Ahnaff Rauf <aaquiff@wso2.com>
2019-03-01 14:32:01 +0530

    Improve deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-CLUSTER_ADMIN_PASSWORD=$1
-WSO2_USERNAME=$2
-WSO2_PASSWORD=$3
+# ------------------------------------------------------------------------
+# Copyright 2017 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+# ------------------------------------------------------------------------
+
+set -e
+
+ECHO=`which echo`
 KUBECTL=`which kubectl`
 
 # methods
@@ -11,16 +27,45 @@ function echoBold () {
 }
 
 function usage () {
-    echoBold "This script automates the installation of Jenkins on a kubernetes cluster"
-    echoBold "Usage: $0 CLUSTER_ADMIN_PASSWORD WUM_USERNAME WUM_PASSWORD"
+    echoBold "This script automates the installation of Jenkins on a kubernetes cluster\n"
+    echoBold "Allowed arguments:\n"
+    echoBold "-h | --help"
+    echoBold "--wu | --wso2-username\t\tYour WSO2 username"
+    echoBold "--wp | --wso2-password\t\tYour WSO2 password"
+    echoBold "--cap | --cluster-admin-password\tKubernetes cluster admin password\n\n"
 }
 
-if [ "$3" = "" ]
-then
-    echoBold "Incorrect Usage!"
-    usage
-    exit 1
-fi
+ADMIN_PASSWORD=''
+WSO2_SUBSCRIPTION_USERNAME=''
+WSO2_SUBSCRIPTION_PASSWORD=''
+
+# capture named arguments
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+
+    case ${PARAM} in
+        -h | --help)
+            usage
+            exit 1
+            ;;
+        --wu | --wso2-username)
+            WSO2_SUBSCRIPTION_USERNAME=${VALUE}
+            ;;
+        --wp | --wso2-password)
+            WSO2_SUBSCRIPTION_PASSWORD=${VALUE}
+            ;;
+        --cap | --cluster-admin-password)
+            ADMIN_PASSWORD=${VALUE}
+            ;;
+        *)
+            echoBold "ERROR: unknown parameter \"${PARAM}\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
 
 echoBold 'Creating namespaces'
 ${KUBECTL} create namespace registry
@@ -37,16 +82,10 @@ echoBold 'Deploying Jenkins'
 ${KUBECTL} config set-context $(kubectl config current-context) --namespace jenkins
 
 echoBold "Creating WUM secret"
-kubectl create secret generic wso2-credentials --from-literal=username=$WSO2_USERNAME --from-literal=password=$WSO2_PASSWORD
+kubectl create secret generic wso2-credentials --from-literal=username=$WSO2_SUBSCRIPTION_USERNAME --from-literal=password=$WSO2_SUBSCRIPTION_PASSWORD
 
-${KUBECTL} apply -f jenkins/roles.yaml --username=admin --password=$CLUSTER_ADMIN_PASSWORD
+${KUBECTL} apply -f jenkins/roles.yaml --username=admin --password=$ADMIN_PASSWORD
 ${KUBECTL} create configmap jenkins-casc-conf --from-file=jenkins/casc_configs/ --dry-run -o yaml | ${KUBECTL} apply -f -
 ${KUBECTL} create configmap jenkins-init-script --from-file jenkins/init.groovy --dry-run -o yaml | ${KUBECTL} apply -f -
 ${KUBECTL} apply -f jenkins/k8s/
 ${KUBECTL} rollout status deployment/jenkins
-
-echoBold 'Waiting for jenkins to start...'
-sleep 30
-
-echo "Jenkins initial admin password:"
-${KUBECTL} exec -it $(${KUBECTL} get pods --selector=app=jenkins --output=jsonpath={.items..metadata.name}) -- cat /var/jenkins_home/secrets/initialAdminPassword
