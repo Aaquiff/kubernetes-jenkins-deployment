4a9ce88d72346ad2ba5318acbb0387ded009100f
Aaquiff Ahnaff Rauf <aaquiff@wso2.com>
2019-03-14 17:17:03 +0530

    Cleanup

--- undeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-KUBECTL=`which kubectl`
-
-${KUBECTL} config set-context $(kubectl config current-context) --namespace jenkins
-${KUBECTL} delete -f jenkins/k8s/deployment.yaml
-${KUBECTL} delete -f jenkins/k8s/service.yaml
-
-${KUBECTL} delete namespace jenkins
