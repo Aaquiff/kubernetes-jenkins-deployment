cfa0b1e52590949c1d9cb55b9f37c6dc089080e3
Aaquiff <aaquiff@wso2.com>
2019-04-08 11:22:30 +0530

    Move helm resources

--- helm/deploy-helm.sh
+++
@@ -1,8 +0,0 @@
-ADMIN_PASSWORD=$1
-
-KUBECTL=`which kubectl`
-HELM=`which helm`
-
-echoBold "Initializing helm in cluster"
-${KUBECTL} apply -f helm/rbac-config.yaml --username=admin --password=$ADMIN_PASSWORD
-${HELM} init --service-account tiller --history-max 200 --upgrade
